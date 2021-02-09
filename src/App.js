d9880f6ef1defc38257ccd3f3f1def5112f9bcd0
Vara Prasad <prasad.marini@gmail.com>
2021-02-09 11:05:49 +0530

    added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { NameForm } from './components/NameForm';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
+      <Title/>
 <NameForm/>
     </div>
   );
